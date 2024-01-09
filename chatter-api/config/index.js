c2298512542eecde34b56a65be9d082cf29e33b5
Phạm Hà Nam <pnam55588@gmail.com>
2024-01-09 19:11:52 +0700

    demo

---
+++ chatter-api/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  AWS_ACCESS_KEY: 'AKIATPYPFU2XPNYMZILL',
+  AWS_SECRET_KEY: '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',
+  AWS_REGION: 'ap-southeast-2',
+  BUCKET: 'nam-bucket-chat',  // s3 bucket name
+  ONESIGNAL_APP: '', // api id
+  ONESIGNAL_KEY: ''
+};
---
+++ chatter-web/src/config/constants.js
@@ -0,0 +1,5 @@
+export default {
+  base_url: 'http://192.168.99.101:8000',
+  bucket_url: 'https://nam-bucket-chat.s3.ap-southeast-2.amazonaws.com',
+  maps_api: ''
+}
